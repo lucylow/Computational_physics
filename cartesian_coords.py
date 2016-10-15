3b41e72989d5f5c594ac77d3a0f9793a3d51d149
Lucy Low <low.lucyy@gmail.com>
2016-10-15 16:01:15 -0400

    Create cartesian_coords.py

---
+++ cartesian_coords.py
@@ -0,0 +1,12 @@
+def cartesian(r,theta):
+	x = r*cos(theta)
+	y = r*sin(theta)
+	position = [x,y] # list with 2 elements
+	return position 
+
+# Note other iterations of the last line work too...
+return[x,y] 
+return array([x,y], float)
+return x,y 
+
+a,b = catesian(r,theta) #this works too 
