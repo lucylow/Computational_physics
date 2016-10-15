973a0b7974fd45d50848074ef96820db79d27c59
Lucy Low <low.lucyy@gmail.com>
2016-10-15 14:55:26 -0400

    Update Rydberg.py

--- Rydberg.py
+++ Rydberg.py
@@ -1,10 +1,30 @@
-# Rydberg formula gives you the first few transitions of the Hydrogen atom 
+'''
+Example 1: Rydberg Equation used in Atomic Physics to find the wavelengths of spectra lines for Hydrogen atoms. 
 
-R = 1.097e-2
+	- inputs: R, m, n, R= constant, m and n are integers representing the principle quantum numbers of the orbitals occupied BEFORE/AFTER the quantum leap 
+
+	- output: invlambda, the first few transitin wavelengths of the EM radiation emitted
+'''
+
+
+R= 1.097e-2
 
 for m in [1,2,3]:
-	print("Series for m=", m)
-	for k in [1,2,3,4,5]:
+	print "Series for m=", m 
+
+	for k in [1,2,3,4,5]: # WHERE K =5 COME FROM???
 		n = m+k
-		invlambda = R*(1/m**2 - 1/n**2)
-		print " ", 1/invlambda," nm"
+		invlambda = R*(1/m**2-1/n**2)
+		print " ",1/invlambda," nm"
+
+
+'''
+('Series for m=', 1)
+  91.1577028259  nm
+  91.1577028259  nm
+  91.1577028259  nm
+  91.1577028259  nm
+  91.1577028259  nm
+('Series for m=', 2)
+
+'''
